9bf779aa8382e05e90b25500656414bb86862e55
Fabry40 <fabr.quaranta@studenti.unina.it>
2025-04-30 11:35:28 +0200

    mo funziona il pulsante

--- client_java/main/src/main/java/com/client/LoginController.java
+++ client_java/main/src/main/java/com/client/LoginController.java
@@ -1,16 +1,23 @@
 package com.client;
 
+
+
 import java.io.IOException;
+
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+
 
 public class LoginController {
     @FXML
     private Button invia_button;
 
     @FXML
-    private void handleButtonClick() {
-        System.out.println("Bottone cliccato!");
+    private void Ciao() throws IOException {
+        System.out.println("Ciao");
+        // Qui puoi aggiungere il codice per gestire l'azione del pulsante
+        // Ad esempio, puoi passare alla schermata principale dell'applicazione
+        // App.setRoot("main");
     }
 }
